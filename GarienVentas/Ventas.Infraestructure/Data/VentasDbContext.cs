13c88eec271f5b7784b5f74857c2331042e88bc3
GaRIEN <gabrielhuaman683@gmail.com>
2026-01-19 23:31:17 -0500

    creacion del context y repositores osea las interfaces en core y repository en infraestructure

--- GarienVentas/Ventas.Infraestructure/Data/VentasDbContext.cs
+++ GarienVentas/Ventas.Infraestructure/Data/VentasDbContext.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
+using Ventas.Core.Entities.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace Ventas.Core.Entities.Models;
+namespace Ventas.Infraestructure.Data;
 
 public partial class VentasDbContext : DbContext
 {
@@ -17,9 +17,6 @@ public partial class VentasDbContext : DbContext
 
     public virtual DbSet<Client> Clients { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=GARIEN-DESKTOP;Database=VentasDb;Trusted_Connection=True;TrustServerCertificate=true;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ GarienVentas/Ventas.Infraestructure/Repositories/ClientRepository.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Ventas.Core.Entities.Models;
+using Ventas.Core.Repositories;
+using Ventas.Infraestructure.Data;
+
+namespace Ventas.Infraestructure.Repositories
+{
+    public class ClientRepository :IClientRepository
+    {
+        private readonly VentasDbContext _context;
+
+        public ClientRepository(VentasDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Client>> GetAllAsync()
+        {
+            return await _context.Clients.ToListAsync();
+        }
+    }
+}
